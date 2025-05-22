ee6d6240580afd66b104f352c125345745e31736
Prahlad10Bhat <prahlad10bhat@gmail.com>
2025-05-22 09:29:56 +0530

    Create Initialization.sh

---
+++ Initialization.sh
@@ -0,0 +1,12 @@
+# Initialize a new repository
+git init
+
+# Add files to staging area
+git add <file>        # Add a specific file
+git add .             # Add all files
+
+# Commit staged changes
+git commit -m "Your commit message"
+
+# Check repository status
+git status
