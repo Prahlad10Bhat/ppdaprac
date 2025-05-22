eccf0392c4a6cd94e0933b6186260bf549fd0d9a
Prahlad10Bhat <prahlad10bhat@gmail.com>
2025-05-22 09:28:43 +0530

    Create Setup.sh

---
+++ Setup.sh
@@ -0,0 +1,9 @@
+# Install Git (example for Ubuntu)
+sudo apt-get install git
+
+# Set global username and email
+git config --global user.name "Your Name"
+git config --global user.email "your@email.com"
+
+# Check configuration
+git config --list
